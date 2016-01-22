432b454246364b9d08e1f7c1c3d5674027b995fe
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-22 15:06:31 +0200

    Create MyTwoProgramm.java

---
+++ MyTwoProgramm.java
@@ -0,0 +1,16 @@
+# kademika_TarasK
+public class MyTwoProgramm {
+	public static void main(String[] args) {
+		String a ="Hello from Java "; //1. Напечатал строку
+		String stroka1 ="Это строка, а ";
+		String stroka2 = "это ее продолжение";
+		int b = 28;
+		String pusto ="        "; //строка содержащая восем пробелов
+		String metka ="перед этим текстом есть 8 пробелов";
+		System.out.print(a); //1. Напечатал строку 4. Использование для вывода System.out.print()
+		System.out.println(" ");
+		System.out.println(stroka1 + stroka2); //2. Сложение двох строк из конкатинацией
+		System.out.println(a + b); //3. Сложение строки из числом
+		System.out.println(pusto+metka); //5. Вывод на экран пустой строки
+	}
+}
