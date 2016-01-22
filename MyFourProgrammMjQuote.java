1c2b50cf9c9da28c1d52505aa457d2cc5560760b
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-22 18:14:20 +0200

    MyFourProgrammMjQuote.java

---
+++ MyFourProgrammMjQuote.java
@@ -0,0 +1,11 @@
+public class MyFourProgrammMjQuote {
+
+	public static void main(String[] args) {
+		String mjQuote = "I'm failed over and over again im my life and that in my life and that is why I succeed!";
+		int a = mjQuote.length();
+		int ch = a - 1;
+		System.out.println("Количество символов в строке: " + a);  
+		System.out.println("Вывод последнего символа из строки " + mjQuote.charAt(ch));
+	}
+
+}
