9bbee75e5f333ea50b203080478a757560efdccc
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-23 21:01:58 +0200

    Program7Replace.java

---
+++ Program7Replace.java
@@ -0,0 +1,8 @@
+public class Program7Replace {
+
+	public static void main(String[] args) {
+		String mtQuote = "Let us always meat each other with smile, for the smile is the beginning of love.";
+		System.out.println(mtQuote.replace("smile", ":)"));
+	}
+
+}
