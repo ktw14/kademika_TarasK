c6ae9157c81798380b30dc87fcaee42431f20b6c
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-23 17:12:14 +0200

    Update MyFiveProgrammSucceed.java

--- MyFiveProgrammSucceed.java
+++ MyFiveProgrammSucceed.java
@@ -1,5 +1,8 @@
-public class MyFiveProgrammSucceed {
+public class MyFiveProgrammSucced {
 
 	public static void main(String[] args) {
 		String mjQuote = "I've failed over and over again im my life and that in my life and that is why I succeed!";
-		boolean s = mjQuote.contains("succeed"); if (s) {System.out.println("succeed");} else {System.out.println("данное слово отсутствует в тексте");}}}
+		System.out.println(mjQuote.substring(mjQuote.length() - "succeed".length()-1, mjQuote.length()-1));
+	}
+
+}
