13b70b8669bb868ddfcc359e47e26046c01fac9f
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-23 20:25:35 +0200

    Programm6Again.java

---
+++ Programm6Again.java
@@ -0,0 +1,9 @@
+
+public class Programm6Again {
+
+	public static void main(String[] args) {
+		String mjQuote = "I've failed over and over again im my life and that in my life and that is why I succeed!";
+		System.out.println(mjQuote.substring(mjQuote.indexOf("again"), mjQuote.length()-57));
+	}
+
+}
