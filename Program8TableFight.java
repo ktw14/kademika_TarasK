7f0bd8e853edfc0229893712fa6d1191a3cdec19
ktw14 <taras.kasiukhnych@gmail.com>
2016-01-24 16:33:48 +0200

    Program8TableFight.java

---
+++ Program8TableFight.java
@@ -0,0 +1,15 @@
+public class Program8TableFight {
+
+	public static void main(String[] args) {
+		System.out.println("BBBBBBBBB");
+		System.out.println("C       C");
+		System.out.println("BR BWBRRB");
+		System.out.println("BRBBWBB B");
+		System.out.println("BWWBRBBBB");
+		System.out.println("B BBRWWB ");
+		System.out.println("BB TRRB R");
+		System.out.println("BBBBBBBBB");
+		System.out.println("BBBBEBBRB");
+	}
+
+}
